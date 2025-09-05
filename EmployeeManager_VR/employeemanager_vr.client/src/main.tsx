848a6256783c26fc321bf7d4babdb2eec034a352
Kurt Shubert <kpshubert@hotmail.com>
2025-09-05 13:04:36 -0500

    20250905 Add deparmtent options to department select. Make sure we're using functional based components (per various recommendations).

--- EmployeeManager_VR/employeemanager_vr.client/src/main.tsx
+++ EmployeeManager_VR/employeemanager_vr.client/src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
+// Ensure the container element exists
+const container = document.getElementById('root');
+
+// Use the non-null assertion operator (!) to satisfy TypeScript if you are certain the element exists
+const root = createRoot(container!);
+
+// Render the App component into the container
+root.render(
     <StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </StrictMode>,
-)
+    </StrictMode>
+);
