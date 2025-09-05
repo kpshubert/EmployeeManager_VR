2ca31afd3542635189ecc67f22aec5e1ac6d7c6f
Kurt Shubert <kpshubert@hotmail.com>
2025-09-05 13:04:36 -0500

    20250905 Add deparmtent options to department select. Make sure we're using functional based components (per various recommendations).

--- EmployeeManager_VR/employeemanager_vr.client/src/App.tsx
+++ EmployeeManager_VR/employeemanager_vr.client/src/App.tsx
@@ -1,19 +1,41 @@
+import { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Department from './Views/Department/Department';
 import Employee from './Views/Employee/Employee';
 import Home from './Views/Home/Home';
 import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/managedepartments' element={<Department />} />
-                <Route path='/manageemployees' element={ <Employee />} />
-            </Routes>
-        </>
-    );
-
+class App extends Component {
+    render() {
+        const content = (
+            <>
+                <nav>
+                    <div className="d-flex justify-content-center">
+                        <div className="row mb-2">
+                            <div className="col-md-auto">
+                                <a href="/">Home</a>
+                            </div>
+                            <div className="col-md-auto">
+                                <a href="/manageemployees">Manage Employees</a>
+                            </div>
+                            <div className="col-md-auto">
+                                <a href="/managedepartments">Manage Departments</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav >
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/managedepartments' element={<Department />} />
+                    <Route path='/manageemployees' element={<Employee />} />
+                </Routes>
+            </>
+        );
+        return (
+            <>
+                {content}
+            </>
+        )
+    }
 }
 
 export default App;
--- EmployeeManager_VR/employeemanager_vr.client/src/Views/Department/Department.tsx
+++ EmployeeManager_VR/employeemanager_vr.client/src/Views/Department/Department.tsx
@@ -1,26 +1,14 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Department.css';
 import type { DepartmentModel } from '../../Models/departmentmodel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSave, faEdit, faWindowClose, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-class Department extends Component {
+function Department() {
 
-    constructor() {
-        super();
-
-        this.state = {
-            departments: null,
-            loading: true,
-            error: null
-        };
-
-        this.onInputChange = this.onInputChange.bind(this);
-        this.onSubmitForm = this.onSubmitForm.bind(this);
-    }
-    departments: DepartmentModel[] = [];
-
-    currentDepartment: DepartmentModel = {
+    //let departments: DepartmentModel[] = [];
+    let currentDepartment: DepartmentModel = {
         id: 0,
         idString: '0',
         name: '',
@@ -28,43 +16,56 @@ class Department extends Component {
         isAssigned: false
     };
 
-    onInputChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-        this.currentDepartment[event.target.name] = event.target.value;
-    }
-
-    onSubmitForm(event) {
-        console.log(this.currentDepartment);
-    }
-
-    componentDidMount() {
-        fetch('/department?id=0&mode=list')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                this.setState({ departments: data, loading: false });
-            })
-            .catch(error => {
-                this.setState({ error: error.message, loading: false });
-            });
-
-    }
-
-    render() {
-        const { departments, loading, error } = this.state;
-
-        if (loading) {
-            return <div>Loading...</div>;
+    const fetchDepartments = async () => {
+        try {
+            const response = await fetch('/department?id=0&mode=list');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setDepartments(data);
+            setLoading(false);
+        } catch (error) {
+            setError(error.message);
+            setLoading(false);
         }
-        if (error) {
-            return <div>Error: {error}</div>;
-        }
+    };
+
+    const loadingDisplay = <div>
+        <div className="row mb-2">
+            <div className="col-md-12">
+                <FontAwesomeIcon icon={faSpinner} className="fa-spin" size="2x"></FontAwesomeIcon>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-12">
+                Loading Department Information...
+            </div>
+        </div>
+    </div>;
+
+    const [departments, setDepartments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Define the fetch function inside useEffect
+    useEffect(() => {
+        fetchDepartments(); // Call the fetch function
+        // Optionally, return a cleanup function if needed
+        return () => {
+            // Cleanup code here if necessary
+        };
+    }, []); // Empty dependency array means this effect runs once, like componentDidMount
+
+    const onInputChange = (event) => {
+    }
+
+    const onSubmitForm = (event) => {
+    }
+
+    const renderTable = () => {
+
+        let returnValue = []
 
         if (departments !== null && departments.length !== 0) {
             const rows = [];
@@ -105,52 +106,52 @@ class Department extends Component {
                         </tr>
                     );
                 }
+            };
+            if (rows !== null && rows.length > 0) {
+                returnValue = rows;
             }
+        }
+        return returnValue;
+    }
 
-            const departmentTable = <table className="table table-striped table-hover table-bordered" aria-labelledby="tableLabel">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Department Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>;
+    const renderTableReturn = renderTable();
 
-            return (
-                <div>
-                    <h1 id="tableLabel">Department Data</h1>
-                    <form>
-                        {departmentTable}
-                        <fieldset className="border border-primary rounded rounded-3 p-2">
-                            <legend>Add/Edit Department</legend>
-                            <div className="row mb-2">
-                                <div className="col-md-12">
-                                    <div className="form-floating mb-1">
-                                        <input name="name" id="name" type="text" onChange={this.onInputChange} placeholder="Department Name" className="form-control" value={this.currentDepartment.name}></input>
-                                        <label className="form-label" htmlFor="name">Department Name</label>
-                                    </div>
+    const departmentTable =
+        <table className="table table-striped table-hover table-bordered" aria-labelledby="tableLabel">
+        <thead>
+            <tr>
+                <th></th>
+                <th>Department Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            {renderTableReturn}
+        </tbody>
+    </table>;
+
+    return (
+        loading ? loadingDisplay :
+            <div>
+                <h1 id="tableLabel">Department Data</h1>
+                <form>
+                    {departmentTable}
+                    <fieldset className="border border-primary rounded rounded-3 p-2">
+                        <legend>Add/Edit Department</legend>
+                        <div className="row mb-2">
+                            <div className="col-md-12">
+                                <div className="form-floating mb-1">
+                                    <input name="name" id="name" type="text" onChange={onInputChange} placeholder="Department Name" className="form-control" value={currentDepartment.name}></input>
+                                    <label className="form-label" htmlFor="name">Department Name</label>
                                 </div>
                             </div>
-                            <button className="btn btn-primary" type="button">
-                                <FontAwesomeIcon icon={faSave} onClick={this.onSubmitForm}></FontAwesomeIcon>&nbsp;Save
-                            </button>
-                        </fieldset>
-                    </form>
-                </div>
-            )
-        }
-
-    }
-    async populateDepartmentData() {
-        const response = await fetch('department?id=0&mode=list');
-        if (response.ok) {
-            const data = await response.json();
-            this.departments = data;
-        }
-    }
+                        </div>
+                        <button className="btn btn-primary" type="button">
+                            <FontAwesomeIcon icon={faSave} onClick={onSubmitForm}></FontAwesomeIcon>&nbsp;Save
+                        </button>
+                    </fieldset>
+                </form>
+            </div>
+    )
 }
 
 export default Department;
